ed03923482062ec4ea6f8361c842275efe0ebef9
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-18 21:07:45 +0700

    menambahkan EntrySiswaBody pada EntrySiswaScreen

--- app/src/main/java/com/example/questapi_074/view/HalamanEntry.kt
+++ app/src/main/java/com/example/questapi_074/view/HalamanEntry.kt
@@ -1,5 +1,9 @@
 package com.example.questapi_074.view
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -11,6 +15,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questapi_074.uicontroller.route.DestinasiEntry
 import com.example.questapi_074.viewmodel.EntryViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,4 +36,20 @@ fun EntrySiswaScreen(
                 scrollBehavior = scrollBehavior
             )
         }
-    )
+    ){ innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.addSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
