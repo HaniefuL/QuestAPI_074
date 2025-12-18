bdc977f3078917aeee933a2cb2a801b19506cbee
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-18 20:48:21 +0700

    menambahkan konfigurasi baseUrl dan HttpLoggingInterceptor

--- app/src/main/java/com/example/questapi_074/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_074/repositori/ContainerApp.kt
@@ -1,9 +1,19 @@
 package com.example.questapi_074.repositori
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 interface ContainerApp {
     // Sediakan dua nama agar tidak error jika ViewModel memanggil salah satunya
     val repositoryDataSiswa: RepositoryDataSiswa
     val repositorySiswa: RepositoryDataSiswa
 }
 
-class DefaultContainerApp: ContainerApp {}
+class DefaultContainerApp: ContainerApp {
+    private val baseurl = "http://10.0.2.2/tiumy/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+
+}
