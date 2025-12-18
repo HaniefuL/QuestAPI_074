0ec21349bbec90ccb8aecc32cd6da1cab0dcec67
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-18 20:43:37 +0700

    menambahkan interface ContainerApp

---
+++ app/src/main/java/com/example/questapi_074/repositori/ContainerApp.kt
@@ -0,0 +1,7 @@
+package com.example.questapi_074.repositori
+
+interface ContainerApp {
+    // Sediakan dua nama agar tidak error jika ViewModel memanggil salah satunya
+    val repositoryDataSiswa: RepositoryDataSiswa
+    val repositorySiswa: RepositoryDataSiswa
+}
