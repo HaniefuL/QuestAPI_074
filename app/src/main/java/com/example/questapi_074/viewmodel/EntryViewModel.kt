1504c0ba08e277b6c499688ccbefb564f102882c
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-18 21:19:23 +0700

    menambahkan EntryViewModel

---
+++ app/src/main/java/com/example/questapi_074/viewmodel/EntryViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.questapi_074.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.questapi_074.repositori.RepositoriDataSiswa
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoriDataSiswa) : ViewModel(){}
