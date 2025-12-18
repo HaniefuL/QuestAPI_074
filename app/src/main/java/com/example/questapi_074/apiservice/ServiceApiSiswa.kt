4fe8167e42c1734ce0c2c9d73cbc30a5316f3419
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-18 12:36:08 +0700

    Menambahkan endpoint POST untuk menambah data siswa

--- app/src/main/java/com/example/questapi_074/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_074/apiservice/ServiceApiSiswa.kt
@@ -3,4 +3,7 @@ package com.example.questapi_074.apiservice
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
