fd2fda6ab58eda39cdbb5603dba0e7c168fa9f86
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-18 21:06:03 +0700

    menambahkan composable EntrySiswaScreen

---
+++ app/src/main/java/com/example/questapi_074/view/HalamanEntry.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_074.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_074.viewmodel.EntryViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
