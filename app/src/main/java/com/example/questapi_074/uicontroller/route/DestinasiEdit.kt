579fbc440c884f5025fbf01a5c532c721ab2f37f
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-26 13:35:24 +0700

    menambahkan titleRes pada DestinasiEdit

--- app/src/main/java/com/example/questapi_074/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_074/uicontroller/route/DestinasiEdit.kt
@@ -2,4 +2,5 @@ package com.example.questapi_074.uicontroller.route
 
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "edit_siswa"
+    override val titleRes = R.string.edit_siswa
 }
