2eede9325a26c8276dd190a2d125c25114293283
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-26 14:27:42 +0700

    menambahkan EntryViewModel pada EditViewModel.kt

--- app/src/main/java/com/example/questapi_074/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_074/viewmodel/EditViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.questapi_074.viewmodel
 
-class EditViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_074.modedata.DetailSiswa
+import com.example.questapi_074.modedata.UIStateSiswa
+import com.example.questapi_074.modedata.toDataSiswa
+import com.example.questapi_074.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EntryViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
