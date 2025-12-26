9aa7b4d6d1e9684f6db8dc073882b223e163c24c
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-26 13:37:29 +0700

    menambahkan routeWithArgs pada DestinasiEdit

--- app/src/main/java/com/example/questapi_074/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_074/uicontroller/route/DestinasiEdit.kt
@@ -1,8 +1,11 @@
 package com.example.questapi_074.uicontroller.route
 
+import com.example.questapi_074.R
+
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "edit_siswa"
     override val titleRes = R.string.edit_siswa
 
     const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
