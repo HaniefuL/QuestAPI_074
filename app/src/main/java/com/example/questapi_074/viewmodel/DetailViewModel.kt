b4e7d7349cc2a6a1e35aeb086b1999fcb7692cad
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-26 14:32:57 +0700

    menambahkan DetailViewModel

---
+++ app/src/main/java/com/example/questapi_074/viewmodel/DetailViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.questapi_074.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_074.modedata.DataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+)
