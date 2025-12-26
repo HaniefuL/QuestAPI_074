332bd3e3b99b86efac17e51e0c33b825b3b59279
HaniefuL <haniefulgeofarry@gmail.com>
2025-12-26 13:35:42 +0700

    menambahkan itemIdArg pada DestinasiEdit

--- app/src/main/java/com/example/questapi_074/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_074/uicontroller/route/DestinasiEdit.kt
@@ -3,4 +3,6 @@ package com.example.questapi_074.uicontroller.route
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "edit_siswa"
     override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
 }
